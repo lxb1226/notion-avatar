4576a9be9f08b067d5d07aaf108515f9b6407d20
lxb1226 <1944303766@qq.com>
2025-07-13 14:41:00 +0800

    fix: add google analytics support and remove google adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,11 +11,11 @@ class Document extends NextDocument {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
-          <script
+          {/* <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3671095188342075"
             crossOrigin="anonymous"
-          />
+          /> */}
           <script
             dangerouslySetInnerHTML={{
               __html: `
